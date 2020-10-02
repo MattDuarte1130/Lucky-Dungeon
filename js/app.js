fcd50931359f5294d01dcba8cdaa412b7436ac5a
Matt Duarte <mattduarte1130@gmail.com>
2020-10-02 13:21:08 -0400

    Got health bar connected to player

--- js/app.js
+++ js/app.js
@@ -1,38 +1,40 @@
 // Class for Player
 class Player {
-  constructor(name,vitality = 100,attackPower = 5,abilityPower = 12, armor = 2){
+  constructor(name,maxVitality = 100, currentHealth, attackPower = 5,abilityPower = 12, armor = 2){
     this.name = name
-    this.vitality = vitality
+    this.maxVitality = maxVitality
+    this.currentHealth = maxVitality
     this.attackPower = attackPower
     this.abilityPower = abilityPower
     this.armor = armor
   }
   useBasicAttack(enemy){
-      enemy.vitality += enemy.armor -= this.attackPower
-      console.log("Your attack hit! " + enemy.name + "'s HP is now at " + enemy.vitality + "!")
+      enemy.currentHealth += enemy.armor -= this.attackPower
+      console.log("Your attack hit! " + enemy.name + "'s HP is now at " + enemy.currentHealth + "!")
     }
     useAbilityPower(enemy){
-        enemy.vitality -= this.abilityPower
-        console.log("Your ability hit! " + enemy.name + "'s HP is now at " + enemy.vitality + "!")
+        enemy.currentHealth -= this.abilityPower
+        console.log("Your ability hit! " + enemy.name + "'s HP is now at " + enemy.currentHealth + "!")
     }
   }
 
 // Class For Enemy
 class Enemy {
-  constructor(name, vitality, attackPower, abilityPower, armor){
+  constructor(name, maxVitality = 75 + Math.floor((Math.random()) * ((90 - 75) + 1)), currentHealth, attackPower, abilityPower, armor){
     this.name = name
-    this.vitality = 75 + Math.floor((Math.random()) * ((90 - 75) + 1))
+    this.maxVitality = maxVitality
+    this.currentHealth = maxVitality
     this.attackPower = 1 + Math.floor((Math.random()) * ((2 - 1) + 1))
     this.abilityPower = 10 + Math.floor((Math.random()) * ((10 - 7) + 1))
     this.armor = 1 + Math.floor((Math.random()) * ((3 - 1) + 1))
   }
   useBasicAttack(enemy){
-      enemy.vitality += enemy.armor -= this.attackPower
-      console.log("Your attack hit! " + enemy.name + "'s HP is now at " + enemy.vitality + "!")
+      enemy.currentHealth += enemy.armor -= this.attackPower
+      console.log("Your attack hit! " + enemy.name + "'s HP is now at " + enemy.currentHealth + "!")
     }
     useAbilityPower(enemy){
-        enemy.vitality -= this.abilityPower
-        console.log("Your ability hit! " + enemy.name + "'s HP is now at " + enemy.vitality + "!")
+        enemy.currentHealth -= this.abilityPower
+        console.log("Your ability hit! " + enemy.name + "'s HP is now at " + enemy.currentHealth + "!")
     }
   }
 
@@ -41,13 +43,26 @@ class Enemy {
 
 
 
-  let playerOne = new Player("USS Schwarzenegger");
+  let playerOne = new Player("Matt");
   let enemyOne = new Enemy("Wimpy Orc");
   console.log(playerOne)
   console.log(enemyOne)
 
   console.log(playerOne.useBasicAttack(enemyOne))
-  console.log(playerOne.useAbilityPower(enemyOne))
+  console.log(enemyOne.useAbilityPower(playerOne))
+
+
+let getPlayerHealthPercentage = playerOne.currentHealth / playerOne.maxVitality * 100 + "%"
+
+let $playerHealthBar = $('.healthBarValue').text(playerOne.currentHealth + "/" + playerOne.maxVitality)
+let $playerHealthBarFill = $('.healthBarFill').css({'width': getPlayerHealthPercentage})
+
+
+
+
+
+
+
 
   $(()=>{
 
